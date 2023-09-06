9d56bfff647b5578aa417cbc5008a287fdb9114c
ummataliyev <djohnummataliyev@gmail.com>
2023-09-06 15:12:51 +0500

    Home Pga Implemented

--- tender_config/settings.py
+++ tender_config/settings.py
@@ -112,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

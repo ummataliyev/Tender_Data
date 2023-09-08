e14ac144825d93052673375c26a545a03f6fe609
ummataliyev <djohnummataliyev@gmail.com>
2023-09-08 14:29:44 +0500

    Changes

--- data_app/models.py
+++ data_app/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Job(models.Model):
     company_name = models.CharField(max_length=255)
+    link = models.TextField()
     is_active = models.BooleanField(default=True)
-    deadline = models.DateTimeField(default=timezone.now)
 
     class Meta:
         verbose_name = "Job"
         verbose_name_plural = "Jobs"
 
     def __str__(self):
-        return f"{self.company_name} - Tender is active: {self.is_active}, Deadline to: {self.deadline}"
+        return f"{self.company_name} - Tender link: {self.link}, Status: {self.is_active}"

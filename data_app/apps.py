12a85655224a618e678b12168e4b5157e6b3b7af
ummataliyev <djohnummataliyev@gmail.com>
2023-09-15 19:35:01 +0500

    Ready For Deployment ✅

--- data_app/apps.py
+++ data_app/apps.py
@@ -1,5 +1,11 @@
+import logging
+
 from django.apps import AppConfig
+from django.db.utils import ProgrammingError
 from django.core.exceptions import ValidationError
+
+
+logger = logging.getLogger(__name__)
 
 
 class DataAppConfig(AppConfig):
@@ -7,36 +13,22 @@ class DataAppConfig(AppConfig):
     name = 'data_app'
 
     def ready(self) -> None:
-        from django_celery_beat.models import PeriodicTask, IntervalSchedule
+        from django_celery_beat.models import PeriodicTask
+        from django_celery_beat.models import IntervalSchedule
         try:
             # Create an interval schedule for the first task
             schedule1, created1 = IntervalSchedule.objects.get_or_create(
                 every=8,
                 period=IntervalSchedule.HOURS,
             )
-            # Create the first periodic task
-            PeriodicTask.objects.get_or_create(
-                interval=schedule1,
-                name='Abd Site Script',
-                task='data_app.tasks.adb_script',
-            )
-
-            # # Create an interval schedule for the second task
-            # schedule2, created2 = IntervalSchedule.objects.get_or_create(
-            #     every=2,  # Change this to the desired interval in seconds
-            #     period=IntervalSchedule.MINUTES,
-            # )
-
-            # # Create the second periodic task
-            # PeriodicTask.objects.get_or_create(
-            #     interval=schedule2,
-            #     name='Xt Site Scripts',
-            #     task='data_app.tasks.xt_script',
-            # )
-
-            # If you want to create a third task, repeat the process with a new schedule and task name.
-
-        except ValidationError:
-            pass
+            if created1:
+                # Create the first periodic task
+                PeriodicTask.objects.get_or_create(
+                    interval=schedule1,
+                    name='Abd Site Script',
+                    task='data_app.tasks.adb_script',
+                )
+        except (ProgrammingError, ValidationError) as exc:
+            logger.error("Ignoring validation and programming error for celery task - %s", exc)
 
         return super().ready()
